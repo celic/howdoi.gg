225740bcad1299ad9d408d03bbcbaba062cb9651
Andrew Rempe <andrewrempe@gmail.com>
2015-07-02 15:44:03 -0400

    Cleans up champion puller

--- lib/pullers/current_champions.rb
+++ lib/pullers/current_champions.rb
@@ -10,16 +10,16 @@ require_relative '../../models/champion'
 db = YAML.load(ERB.new(File.read(File.join('config','database.yml'))).result)
 api_key = db['api']['key']
 
-current_champs_json = open('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=info&api_key=#{api_key}')
+current_champs_json = open("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=info&api_key=#{api_key}")
 current_champs_full = JSON.parse current_champs_json.read
 
 champ_list = current_champs_full['data']
 
 # Add list of champions
-champ_list.each do |champ|
+champ_list.each do |name, champ|
 
 	# Check if champion is already in the db
-	if Champion.find_by name: champ['name'] == nil
-		Champion.create(riot_id: champ['id'], name: champ['name'], desc: champ['title'])
+	unless Champion.find_by name: champ['name']
+		Champion.create(riot_id: champ['id'], name: name, desc: ['title'])
 	end
 end
