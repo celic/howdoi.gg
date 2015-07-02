ac50fb145f527905797d1941f9e1d24d9c1ee29e
celic <cceli@codequarry.net>
2015-07-02 00:16:55 -0400

    Adds config file

--- models/champion.rb
+++ models/champion.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: champions
+#
+#  id         :integer          not null, primary key
+#  riot_id    :integer
+#  name       :string
+#  desc       :string
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Champion < ActiveRecord::Base
 
 end
