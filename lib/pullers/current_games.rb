2bb56e29d05881b6a468d834999e4117ceb64820
celic <cceli@codequarry.net>
2015-07-03 01:32:41 -0400

    Adds some testing capabilities to not max out api key

--- lib/pullers/current_games.rb
+++ lib/pullers/current_games.rb
@@ -11,11 +11,16 @@ require_relative '../../models/game'
 db = YAML.load(ERB.new(File.read(File.join('config','database.yml'))).result)
 api_key = db['api']['key']
 
-Players.all.each do |player|
+test_players = Players.all(1..5)
+
+test_players.all.each do |player|
 
 	player_id = player.riot_id
 	player_games_json = open("https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/#{player_id}/recent?api_key=#{api_key}")
 	player_games_full = JSON.parse player_games_json.read
 	player_games_list = player_games_full['games']
 
+	player_games_list.each do |game|
+
+	end
 end
