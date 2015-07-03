f13a043d9149f65cdf0151ad8da25bdee4269c20
Andrew Rempe <andrewrempe@gmail.com>
2015-07-02 22:57:22 -0400

    Adds a Champion page endpoint

--- app.rb
+++ app.rb
@@ -13,3 +13,17 @@ get '/' do
 
 	erb :'pages/home'
 end
+
+get '/champions/:key' do
+
+	key = params[:key].downcase
+
+	@champion = Champion.where("lower(key) = ?", key).first
+
+	unless @champion
+
+		# some sort of 404
+	end
+
+	erb :'champions/show'
+end
